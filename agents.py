95f755e3d34010b910a2043fb881d69bdc3bd473
Yavuz <yavuz.kulaber@excelra.com>
2024-06-05 22:04:49 +0100

    tasks initial description to be improved

--- agents.py
+++ agents.py
@@ -25,13 +25,27 @@ class SkillAgent():
                 BrowserTools.scrape_and_summarize_website
             ],
             verbose: True)
+    
     def SkillSage(self):
         return Agent(
-            role: "Requirement analyst",
-            goal: "Investigate the requirements from job description",
-            backstory= "I am a requirement analyst expert who is looking for the requirements and essential skills in the job description",
+            role: "Skill assessor",
+            goal: "Assess the essential skills required by highest paid jobs",
+            backstory= "I am a skill assesor expert who is extracting essential skills
+              "required by the highest paid jobs",
             tools: [
                 SearchTools.searh_internet,
                 BrowserTools.scrape_and_summarize_website
             ],
             verbose: True)
+    def SuccessJourney(self):
+        return Agent(
+            role: "Roadmap creator expert",
+            goal: "Creates detailed roadmaps career progression",
+            backstory= " Creates detailed roadmaps for projects or career progression, ensuring clear,"
+              "step-by-step guidance towards goals",
+            tools: [
+                SearchTools.searh_internet,
+                BrowserTools.scrape_and_summarize_website
+            ],
+            verbose: True)
+    
